fc974ed28f864da8a65dfa5be583fd0fbf88c125
Henrik Kiepe <drgoodcat77@googlemail.com>
2023-10-20 13:14:31 +0000

    corrected constants

--- src/components/footers/FiveColumnWithInputForm.js
+++ src/components/footers/FiveColumnWithInputForm.js
@@ -21,7 +21,7 @@ const Column = tw.div`px-4 sm:px-0 sm:w-1/4 md:w-auto mt-12`;
 //const LinkListItem = tw.li`mt-3`;
 //const Link = tw.a`border-b-2 border-transparent hocus:border-gray-700 pb-1 transition duration-300`;
 
-const SubscribeNewsletterColumn = tw(
+/* const SubscribeNewsletterColumn = tw(
   Column
 )`text-center lg:text-left w-full! lg:w-auto! mt-20 lg:mt-12`;
 const SubscribeNewsletterContainer = tw.div`max-w-sm mx-auto lg:mx-0 `;
@@ -30,7 +30,7 @@ const SubscribeForm = tw.form`mt-4 lg:mt-6 text-sm sm:flex max-w-xs sm:max-w-non
 const Input = tw.input`bg-gray-300 px-6 py-3 rounded sm:rounded-r-none border-2 sm:border-r-0 border-gray-400 hover:border-primary-500 focus:outline-none transition duration-300 w-full`;
 const SubscribeButton = tw(
   PrimaryButtonBase
-)`mt-4 sm:mt-0 w-full sm:w-auto rounded sm:rounded-l-none px-8 py-3`;
+)`mt-4 sm:mt-0 w-full sm:w-auto rounded sm:rounded-l-none px-8 py-3`; */
 
 const Divider = tw.div`my-16 border-b-2 border-gray-300 w-full`;
 
